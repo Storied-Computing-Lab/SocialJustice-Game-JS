7ebb2bfd683224a87dce6ba8b38ba6bf25490b44
Ashish Shrestha <ashishshrestha13@gmail.com>
2021-02-18 14:42:08 -0800

    initial commit, set up Monogatari settings and implemented up to Screen 8

---
+++ js/chapters/a_The_Center.js
@@ -0,0 +1,12 @@
+monogatari.script ({
+    'a_The_Center': [
+        "clear",
+        "centered You walk into the Center for Chamoru Rights and look out the window.",
+        "centered The green fronds of palm trees sway lazily in the wind.",
+        "centered An innoncence and beauty that frustrates you",
+        "centered Because you don't know how to protect it.",
+        "centered You shut your eyes for a moment to hold the picture in your mind.",
+        "c The U.S. military buildup and bombing is set to steal and destroy even more of our ancestral homeland",
+        "c What can my people do to stop this?",
+    ]
+})
---
+++ js/script.js
@@ -0,0 +1,98 @@
+/* global monogatari */
+
+// Define the messages used in the game.
+monogatari.action ('message').messages ({
+	'Help': {
+		title: 'Help',
+		subtitle: 'Some useful Links',
+		body: `
+			<p><a href='https://developers.monogatari.io/documentation/'>Documentation</a> - Everything you need to know.</p>
+			<p><a href='https://monogatari.io/demo/'>Demo</a> - A simple Demo.</p>
+		`
+	}
+});
+
+// Define the notifications used in the game
+monogatari.action ('notification').notifications ({
+	'Welcome': {
+		title: 'Welcome',
+		body: 'This is the Monogatari VN Engine',
+		icon: ''
+	}
+});
+
+// Define the Particles JS Configurations used in the game
+monogatari.action ('particles').particles ({
+
+});
+
+// Define the canvas objects used in the game
+monogatari.action ('canvas').objects ({
+
+});
+
+// Credits of the people involved in the creation of this awesome game
+monogatari.configuration ('credits', {
+
+});
+
+
+// Define the images that will be available on your game's image gallery
+monogatari.assets ('gallery', {
+
+});
+
+// Define the music used in the game.
+monogatari.assets ('music', {
+
+});
+
+// Define the voice files used in the game.
+monogatari.assets ('voices', {
+
+});
+
+// Define the sounds used in the game.
+monogatari.assets ('sounds', {
+
+});
+
+// Define the videos used in the game.
+monogatari.assets ('videos', {
+
+});
+
+// Define the images used in the game.
+monogatari.assets ('images', {
+
+});
+
+// Define the backgrounds for each scene.
+monogatari.assets ('scenes', {
+
+});
+
+
+// Define the Characters
+monogatari.characters ({
+	// 'y': {
+	// 	name: 'Yui',
+	// 	color: '#5bcaff'
+	// }
+
+	'c': {
+		name: 'Clara',
+		color: '#008080'
+	}
+});
+
+monogatari.script ({
+	
+	// The game starts here.
+
+	// jump to first chapter
+	'Start': [
+		"jump a_The_Center",
+	]
+
+});
