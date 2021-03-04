0bdae3e9679a6f930f12c02bfb34b4595aae5d4e
Ashish Shrestha <ashishshrestha13@gmail.com>
2021-03-04 15:49:38 -0800

    Hack 1 finished, determine how to implement the hacks

---
+++ js/chapters/hack_1.js
@@ -0,0 +1,39 @@
+// import { main } from "../hacks/niko_wuz_here.js";
+
+function check_nikos_hack () {
+    // check if nikos hack was fixed
+    console.log('hack_1')
+
+    if (main() != "Niko wuz here") {
+        monogatari.storage({
+            hack_1: true,
+        });
+    }
+    
+    return
+}
+
+monogatari.script({
+    'cond': [
+        "show scene the_center",
+        'c I am still locked out of my computer',
+        {'Conditional': {
+            'Condition': () => {
+                return  monogatari.storage('hack_1');
+            },
+            'True': 'jump fixed',
+            'False': 'jump cond',
+        }},
+    ],
+
+    // 'not_fixed': [
+    //     'c I am still locked out of my computer',
+    //     'jump cond',
+    // ],
+
+    'fixed': [
+        'c You got in your computer!'
+    ],
+
+
+})
