af3094a718f7c5423c125663fa9b4a1558b77c6c
Zaynab At-Taras <zattaras@berkeley.edu>
2021-06-02 15:50:20 -0700

    standarization my_function() {does something cool} thing_runs_my_fuction() {my_function(); } 

--- js/hacks/hack 4/hack_4_function.js
+++ js/hacks/hack 4/hack_4_function.js
@@ -1,18 +1,22 @@
  /*
     This file is where you are going to define your function/ code for this hack.
  */
+
  var funcCount = 0;
- function hack_4_function(){
-    // uncomment the line below if you design your hack 4 to need to keep track of how many times the function is called
-    funcCount++;
-    return "test";
+ function hack4(){
+     funcCount++;
+     //return funcCount if want calls
+     return "test";
  }
+ function hack_4_function(){
+    //add a for loop if want to use calls version(what in iterate)
+     return hack4();
+  }
 
-
-function iterate (){
-   for (var i = 0; i < 5; i++){
-        hack_4_function();
-        console.log(funcCount);
-    }
-    return funcCount;
-}
+ function iterate (){
+    for (var i = 0; i < 5; i++){
+         hack_4_function();
+         console.log(funcCount);
+     }
+     return funcCount;
+ }
