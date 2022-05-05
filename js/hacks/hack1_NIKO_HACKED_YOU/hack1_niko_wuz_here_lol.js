c734641513167aceb5713a8e4985488bd9b5beb5
Ruben Pulido <rubenpulido@berkeley.edu>
2022-05-04 19:50:20 -0700

    Cleaning up comments

--- js/hacks/hack1_NIKO_HACKED_YOU/hack1_niko_wuz_here_lol.js
+++ js/hacks/hack1_NIKO_HACKED_YOU/hack1_niko_wuz_here_lol.js
@@ -1,5 +1,5 @@
-//CAREFULLY READ COMMENTS, All text in gray is a comment.
 /*
+CAREFULLY READ COMMENTS, All text in gray is a comment.
 In JavaScript computers do not read '//' which is used to comment a single line.
 To comment out multiple lines, use /*.
 */
@@ -22,13 +22,17 @@ function main () {
 
 
 /*
+NOTE! THE FOLLOWING FUNCTIONS BELOW ARE NOT REQUIRED TO SOLVE HACK 1 THEY ARE BONUS CHALLENGES!
+
 The following code below are functions, which are a block of code designed to perform
 a particular tasks. A function is executed when it is called
 */
 
 function eliminateVirus() {
-    // When this function is called it will return the string below
-    // How to use: write eliminateVirus after the return statement in the main function
+    /*
+    When this function is called it will return the string below
+    How to use: write eliminateVirus after the return statement in the main function
+    */
     return "Virus has been eliminated";
 }
 
@@ -37,12 +41,9 @@ function nameYourFunction(message) {
     /*
     Functions can also take arguments which can be numbers, strings, etc. In this case
     the argument is named "message" but it can be named anything.
-    */
 
-    /*
     How to use: Figure out how to use the argument inside the function and calling it
     is very similar to the manner explained in the last function.
     */
-
     return ;
 }
